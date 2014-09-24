1b8d6eb3f886ed69840950b0900ac66186a4d51d
Hiroyuki Sano <sh19910711@gmail.com>
2014-09-24 14:57:13 +0900

    Add func.cpp

---
+++ src/func.cpp
@@ -0,0 +1,11 @@
+// @snip<func/sub_func_1.cpp>
+// @snip<func/sub_func_2.cpp>
+// @snip<func/sub_func_3.cpp>
+
+int func() {
+  int res = 0;
+  res += sub_func_1();
+  res += sub_func_2();
+  res += sub_func_3();
+  return res;
+}
